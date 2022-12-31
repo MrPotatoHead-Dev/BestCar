5bd6c9b69b7e09404eb5628f00b88b7027ad782d
MrPotatoHead-Dev <121565225+MrPotatoHead-Dev@users.noreply.github.com>
2022-12-31 10:38:17 -0300

    Update BestCar.sol

--- BestCar.sol
+++ BestCar.sol
@@ -5,11 +5,13 @@ pragma solidity ^0.8.0;
 contract Name_Best_Car {
     // More specifically 1985 Suzuki Samurai
     string private BestCar = "Suzuki Samurai";
-
+    
+    // A function that converts strings to bytes32
     function byteView(string memory _random) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(_random));
     }
-
+    // The function where an input is used to compare
+    // with my answer
     function guess(string memory _BestCar) public view returns (bool) {
         // Compare their byte type
         return
